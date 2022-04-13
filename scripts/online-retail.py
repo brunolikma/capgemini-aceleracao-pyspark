05313dd2d840baca996b9e85594d263455314393
brunolikma <brunosssss@gmail.com>
2022-04-13 10:50:53 -0300

    add questions 6 and 7

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -27,7 +27,7 @@ def question_1_qa_InvoiceNo (df):
 
 def question_2_qa_StockCode (df):
 	df = df.withColumn("qa_StockCode", (
-		F.when(~F.col("StockCode").rlike("([0-9a-zA-Z]{5})"), "Unknown")
+		F.when(~F.col("StockCode").rlike("([0-9a-zA-Z]{5})"), "Missing")
 		.otherwise("Effective")
 	))
 	print(df.groupBy("qa_StockCode").count().show())
@@ -36,8 +36,8 @@ def question_2_qa_StockCode (df):
 
 def question_3_qa_Description (df):
 	df = df.withColumn("qa_Description", (
-		F.when(F.col("Description").isNull(), "Unknown")
-		.when(F.col("Description") == "", "Unknown")
+		F.when(F.col("Description").isNull(), "Missing")
+		.when(F.col("Description") == "", "Missing")
 		.otherwise("Effective")
 		))
 	print(df.groupBy("qa_Description").count().show())
@@ -57,6 +57,22 @@ def question_5_qa_UnitPrice (df):
 	F.col("UnitPrice").cast(FloatType())))
 	print(df.printSchema())
 
+#Pergunta QA 6
+
+def question_6_qa_CustomerID(df):
+	df = df.withColumn("qa_CustomerID", ()
+	F.when(~F.col("CustomerID").rlike("([0-9a-zA-Z]{5})"), "Failure").otherwise("Effective")))
+	print(df.groupBy("qa_CustomerID").count().distinct().orderBy("qa_CustomerID", ascending=False).show())
+
+#Pergunta QA 7
+
+def question_7_qa_Country(df):
+	df = df.withColumn("qa_Country", (
+	F.when(F.col("Country").isNull(), "Missing")
+	 .when(F.col("Country") == "", "Missing")
+	 .otherwise("Effective")))
+	print(df.groupBy("qa_Country").count().distinct().orderBy("qa_Country", ascending=False).show()) 
+
 #Função para teste
 
 def testing (df):
@@ -71,7 +87,8 @@ def Final_Boss (df):
 	question_3_qa_Description(df)
 	question_4_qa_InvoiceDate(df)
 	question_5_qa_UnitPrice(df)
-
+	question_6_qa_CustomerID(df)
+	question_7_qa_Country(df)
 
 if __name__ == "__main__":
 	sc = SparkContext()
