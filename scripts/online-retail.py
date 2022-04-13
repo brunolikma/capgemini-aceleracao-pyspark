dea55333da2c2f353839b9d882d5aaf5b1a20b7e
brunolikma <brunosssss@gmail.com>
2022-04-13 10:44:56 -0300

    fix issue 4 and add to 5

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -13,6 +13,7 @@ schema_online_retail = StructType([
 	StructField("CustomerID",  IntegerType(),  True),
 	StructField("Country",  StringType(),  True),
 ])
+# Pergunta QA 1
 
 def question_1_qa_InvoiceNo (df):
 	df = df.withColumn('qa_InvoiceNo', (
@@ -22,12 +23,16 @@ def question_1_qa_InvoiceNo (df):
 	))
 	print(df.groupBy("qa_InvoiceNo").count().show())
 
+# Pergunta QA 2
+
 def question_2_qa_StockCode (df):
 	df = df.withColumn("qa_StockCode", (
 		F.when(~F.col("StockCode").rlike("([0-9a-zA-Z]{5})"), "Unknown")
 		.otherwise("Effective")
 	))
 	print(df.groupBy("qa_StockCode").count().show())
+
+#Pergunta QA 3
 
 def question_3_qa_Description (df):
 	df = df.withColumn("qa_Description", (
@@ -37,14 +42,35 @@ def question_3_qa_Description (df):
 		))
 	print(df.groupBy("qa_Description").count().show())
 
+#Pergunta QA 4
+
+def question_4_qa_InvoiceDate (df):
+	df = df.withColumn("InvoiceDate",(
+		F.to_timestamp(F.col("InvoiceDate"), "d/M/yyyy H:m")
+	))
+	print(df.printSchema())
+
+#Pergunta QA 5
+
+def question_5_qa_UnitPrice (df):
+	df = df.withColumn("UnitPrice", (
+	F.col("UnitPrice").cast(FloatType())))
+	print(df.printSchema())
+
+#Função para teste
 
 def testing (df):
 	pass
+
+
+#Função final para executar outras funções
 
 def Final_Boss (df):
 	question_1_qa_InvoiceNo(df)
 	question_2_qa_StockCode(df)
 	question_3_qa_Description(df)
+	question_4_qa_InvoiceDate(df)
+	question_5_qa_UnitPrice(df)
 
 
 if __name__ == "__main__":
@@ -57,8 +83,8 @@ if __name__ == "__main__":
 		          .schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
 	#print(df.printSchema())
-	#testCoding(df)
 	#question_1_qa_InvoiceNo(df)
 	#pergunta_1_qa(df)
 	#print(df_final.show(999999))
 	Final_Boss (df)
+	#testing(df)
